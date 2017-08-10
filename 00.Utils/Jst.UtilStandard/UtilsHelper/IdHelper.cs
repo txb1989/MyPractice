bc303546a089be48b37c0b1ca5181d783db71bc4
txb1989 <805513839@qq.com>
2017-08-10 18:46:14 +0800

    Id生产

---
+++ 00.Utils/Jst.UtilStandard/UtilsHelper/IdHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jst.UtilStandard.UtilsHelper
+{
+    public class IdHelper
+    {
+        public static long GetId()
+        {
+            byte[] bytes = Guid.NewGuid().ToByteArray();
+            long id =  BitConverter.ToInt64(bytes, 0);
+            return id;
+        }
+
+    }
+}
---
+++ 09.UniteTest/Utils.CoreUniteTest/CoreUtilsUniteTest.cs
@@ -0,0 +1,19 @@
+﻿using Jst.UtilStandard.UtilsHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils.CoreUniteTest
+{
+    [TestClass]
+    public class CoreUtilsUniteTest
+    {
+        [TestMethod]
+        public void TestGetId()
+        {
+            var id = IdHelper.GetId();
+            Assert.AreNotEqual(id, 0);
+        }
+    }
+}
