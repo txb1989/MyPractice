8774ed5d0defd59709cf18dd9bce0f9c18d7eada
txb1989 <805513839@qq.com>
2017-08-11 14:17:55 +0800

    想试试AspectCore切面的东西

---
+++ 10.Study/AspectCoreStudy/CustomerInterceptorAttribute.cs
@@ -0,0 +1,12 @@
+﻿using AspectCore.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspectCoreStudy
+{
+    public class CustomerInterceptorAttribute : InterceptorAttribute
+    {
+
+    }
+}
